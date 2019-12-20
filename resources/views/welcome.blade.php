0b27d759c8c456eca12ad4c19c254d675ed0c6a5
Dominik Fomicz <admin@nc-dev.pl>
2019-12-20 19:35:48 +0100

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Foodini
+                    Foodini d
                 </div>
 
                 <div class="links">
