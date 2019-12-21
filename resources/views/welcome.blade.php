b208807f9d71b63091864487b065f1ff60fe359f
Dominik Fomicz <admin@nc-dev.pl>
2019-12-21 16:21:49 +0100

    normalo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Foodini 321111
+                    Foodini Push Push
                 </div>
             </div>
         </div>
