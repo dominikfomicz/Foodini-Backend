2e3bd3b6869c27e065fab18a4fe1c1b7eee7a00b
Dominik Fomicz <admin@nc-dev.pl>
2019-12-22 15:43:32 +0100

    rejestracja po rest

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+	protected function generateAccessToken($user){
+		$token = $user->createToken($user->email.'-'.now());
+
+		return $token->accessToken;
+	}
+
+
+	public function register(Request $request){
+		$request->validate([
+			'name' => 'required',
+			'email' => 'required',
+			'password' => 'required'
+		]);
+
+
+		$user = User::create([
+			'name' => $request->name,
+			'email' => $request->email,
+			'password' => bcrypt($request->password)
+		]);
+
+		return response()->json($user);
+	}
+}
