5947ccb33798b0992788e46f7aa5ebe74f341107
krosfiin <mszymaniec@interia.eu>
2019-12-24 12:47:39 +0100

    mm

--- app/Http/Services/Tools/SelectItemService.php
+++ app/Http/Services/Tools/SelectItemService.php
@@ -9,6 +9,18 @@ use DB;
 class SelectItemService {
 
     //
+    private function convertToReturnArray($array_list, $item_id_prop, $item_name_prop) {
+        $return_array = [];        
+        foreach ($array_list as $item) {
+            $tmpItem = new \stdClass();
+            $tmpItem->item_id = $item->{$item_id_prop};
+            $tmpItem->item_name = $item->{$item_name_prop};
+            $return_array[] = $tmpItem;
+        }
+
+        return $return_array;
+    }
+
     public function getList($input_array) {
         $method = "get{$input_array['app_list_string']}";
         if (is_callable(array($this, $method))) {
@@ -25,20 +37,6 @@ class SelectItemService {
         return $return;
     }
 
-    //
-    private function convertToReturnArray($array_list, $item_id_prop, $item_name_prop) {
-        $return_array = [];        
-        foreach ($array_list as $item) {
-            $tmpItem = new \stdClass();
-            $tmpItem->item_id = $item->{$item_id_prop};
-            $tmpItem->item_name = $item->{$item_name_prop};
-            $return_array[] = $tmpItem;
-        }
-
-        return $return_array;
-    }
-
-    // 
     private function getCityConstType() {
         //$list = \App\Models\s_case\HistoryConstType::where("visible", "1")->select("id", "name")->get();
         $list = CityConstType::select("id", "name")->get();
