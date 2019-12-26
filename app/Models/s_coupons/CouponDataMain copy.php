5b29469dcb35cf235a98fcaeaf8fdb11fa74d609
Dominik Fomicz <admin@nc-dev.pl>
2019-12-26 19:15:28 +0100

    213

---
+++ app/Models/s_coupons/CouponDataMain copy.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Models\s_coupons;
+use Illuminate\Database\Eloquent\Model;
+
+class CouponDataMain extends Model {
+    
+    protected $table = "s_coupons.t_coupon_data_main";
+    public $timestamps = false;
+    
+    protected $fillable = [
+    ];    
+}
