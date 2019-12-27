6efd5955bb95a7a3d134bb4d604c3800fb692185
krosfiin <mszymaniec@interia.eu>
2019-12-27 17:40:44 +0100

    mm

--- app/Http/Repositories/Coupons/CouponsRepository.php
+++ app/Http/Repositories/Coupons/CouponsRepository.php
@@ -34,7 +34,7 @@ class CouponsRepository
                         ELSE FALSE
                         END AS status,
                         CASE WHEN c.amount = -1 THEN c.amount
-                            WHEN c.amount - used_counter.used_counter IS NOT NULL THEN c.amount - used_counter.used_counter
+                            WHEN used_counter.used_counter IS NOT NULL THEN c.amount - used_counter.used_counter
                         ELSE c.amount
                         END AS coupon_left
                     FROM s_coupons.t_coupon_data_main c
