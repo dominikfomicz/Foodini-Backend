addf81cf0e098410e46e3ed9dce96e2b4a59e271
krosfiin <mszymaniec@interia.eu>
2019-12-30 21:19:40 +0100

    mm

--- app/Models/s_locals/LocalRefDocument.php
+++ app/Models/s_locals/LocalRefDocument.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class LocalRefDocument extends Model {
     
-    protected $table = "s_locals.t_document_ref_document";
+    protected $table = "s_locals.t_local_ref_document";
     public $timestamps = false;
     
     protected $fillable = [
