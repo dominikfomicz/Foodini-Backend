0a941f41997d0145f1523c862d65806f2a9da2b2
Dominik Fomicz <admin@nc-dev.pl>
2020-01-01 15:26:21 +0100

    error test

--- app/Models/s_locals/LocalDataMain.php
+++ app/Models/s_locals/LocalDataMain.php
@@ -3,10 +3,10 @@ namespace App\Models\s_locals;
 use Illuminate\Database\Eloquent\Model;
 
 class LocalDataMain extends Model {
-    
-    protected $table = "s_locals.t_local_data_main";
+
+    protected $table = "s_locals.t_local_data_mainn";
     public $timestamps = false;
-    
+
     protected $fillable = [
-    ];    
+    ];
 }
