cb72fb5c0a1e66033d04ba6002fdc623c5386812
Dominik Fomicz <admin@nc-dev.pl>
2020-01-01 16:04:47 +0100

    jh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
                             @endif
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('log-viewer/logs') }}">{{ __('Logs') }}</a>
+                                <a class="nav-link" href="log-viewer/logs">Logs</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
