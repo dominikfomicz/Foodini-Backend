21db7fd3462bb8bc59bcaa149fc1b17043a7f4af
krosfiin <mszymaniec@interia.eu>
2020-01-02 18:50:03 +0100

    mm

--- app/Http/Services/Locals/FilesService.php
+++ app/Http/Services/Locals/FilesService.php
@@ -9,19 +9,19 @@ class FilesService
     public function addLogo($id_local_data_main, $file){
         $file_name = uniqid().".png";
 
-        $img = imagescale( $file, 64, 64 ); 
+        $img = imagescale( file_get_contents($file), 64, 64 ); 
 
 
         $doc = New DocumentDataMain();
         $doc->id_document_const_type = 1;
         $doc->file_name = $file_name;
         $doc->save();
-        
+
         $ref = new LocalRefDocument();
         $ref->id_local_data_main = $id_local_data_main;
         $ref->id_document_data_main = $doc->id;
         $ref->save();
         $filePath = "public/locals/".$id_local_data_main."/".$file_name;
-        Storage::disk('local')->put($filePath, file_get_contents($img));
+        Storage::disk('local')->put($filePath, $img);
     }
 }
