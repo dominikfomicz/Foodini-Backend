9b657b85876361ca0fdc1bb355cc3e00e181e943
krosfiin <mszymaniec@interia.eu>
2020-01-02 19:37:55 +0100

    mm

--- app/Http/Services/Locals/FilesService.php
+++ app/Http/Services/Locals/FilesService.php
@@ -9,6 +9,11 @@ class FilesService
     public function addLogo($id_local_data_main, $file){
         $file_name = uniqid().".png";
 
+        $image = Image::make($file);
+
+        $image->fit(64, 64, function ($constraint) {
+            $constraint->aspectRatio();
+        });
 
         $doc = New DocumentDataMain();
         $doc->id_document_const_type = 1;
@@ -20,30 +25,7 @@ class FilesService
         $ref->id_document_data_main = $doc->id;
         $ref->save();
         $filePath = "public/locals/".$id_local_data_main."/".$file_name;
-        Storage::disk('local')->put($filePath, file_get_contents($file));
+        Storage::disk('local')->put($filePath, $image->encode());
 
-        $this->createThumbnail($filePath);
-    }
-
-    public function createThumbnail($file_path) {
-        $file_path_orginal = "../storage/app/".$file_path;
-        // download and create gd image
-        $image = ImageCreateFromString(file_get_contents($file_path_orginal));
-
-        // calculate resized ratio
-        // Note: if $height is set to TRUE then we automatically calculate the height based on the ratio
-        $width = 64;
-        $height = 64;
-        // create image 
-        $output = ImageCreateTrueColor($width, $height);
-        ImageCopyResampled($output, $image, 0, 0, 0, 0, $width, $height, ImageSX($image), ImageSY($image));
-
-        // save image
-        ob_start();
-        ImageJPEG($output, null, 80);
-        $contents = ob_get_contents();
-        ob_end_clean();
-        // return resized image
-        Storage::disk('local')->put($file_path, file_get_contents($contents));
     }
 }
