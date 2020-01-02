a670332ed8fa42f465af581f4a47709f9abda941
Dominik Fomicz <admin@nc-dev.pl>
2020-01-02 12:52:22 +0100

    sd

--- app/Http/Services/Locals/FilesService.php
+++ app/Http/Services/Locals/FilesService.php
@@ -14,7 +14,7 @@ class FilesService
 
         $image = imagepng(imagecreatefromstring(file_get_contents($file)), $file_name);
 
-        // $image = resize_image($image, 128, 128);
+        $image = $this->resize_image($image, 128, 128);
 
         $doc = New DocumentDataMain();
         $doc->id_document_const_type = 1;
@@ -29,5 +29,32 @@ class FilesService
         $filePath = "locals/".$id_local_data_main."/".$file_name;
         Storage::disk('local')->put($filePath, file_get_contents($file));
         return $filePath;
+    }
+
+    public function resize_image($file, $w, $h, $crop=FALSE) {
+        list($width, $height) = getimagesize($file);
+        $r = $width / $height;
+        if ($crop) {
+            if ($width > $height) {
+                $width = ceil($width-($width*abs($r-$w/$h)));
+            } else {
+                $height = ceil($height-($height*abs($r-$w/$h)));
+            }
+            $newwidth = $w;
+            $newheight = $h;
+        } else {
+            if ($w/$h > $r) {
+                $newwidth = $h*$r;
+                $newheight = $h;
+            } else {
+                $newheight = $w/$r;
+                $newwidth = $w;
+            }
+        }
+        $src = imagecreatefromjpeg($file);
+        $dst = imagecreatetruecolor($newwidth, $newheight);
+        imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+        return $dst;
     }
 }
