4a2a29d979db42e783160048c25b6ab06d74ea01
krosfiin <mszymaniec@interia.eu>
2020-01-06 14:29:00 +0100

    mm

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -20,7 +20,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/auth-api/register',
-        '/auth-api/registerUuid'
-
+        '/auth-api/registerUuid',
+        '/auth-api/loginUuid'
     ];
 }
