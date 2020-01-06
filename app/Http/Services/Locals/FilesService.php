1b21185fc2542f4df606db97838d9c7da828f99a
Dominik Fomicz <admin@nc-dev.pl>
2020-01-06 12:38:18 +0100

    df

--- app/Http/Services/Locals/FilesService.php
+++ app/Http/Services/Locals/FilesService.php
@@ -35,7 +35,7 @@ class FilesService
 
         $image = Image::make($file);
 
-        $image->resize(500, null, function ($constraint) {
+        $image->resize(700, null, function ($constraint) {
             $constraint->aspectRatio();
         });
 
