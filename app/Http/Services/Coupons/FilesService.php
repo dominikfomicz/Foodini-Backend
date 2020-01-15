785faf808b2ec1e97d1cebce434c2a24169de14c
krosfiin <mszymaniec@interia.eu>
2020-01-15 07:29:00 +0100

    mm

--- app/Http/Services/Coupons/FilesService.php
+++ app/Http/Services/Coupons/FilesService.php
@@ -14,16 +14,20 @@ class FilesService
         If (Auth::user()->user_type == -1){
             // $file_name = uniqid().".svg";
             $file_name = "logo.svg";
+            $ref = CouponRefDocument::where('id_coupon_data_main', $id_coupon_data_main)->first();
             //kom
-            $doc = New DocumentDataMain();
-            $doc->id_document_const_type = 4;
-            $doc->file_name = $file_name;
-            $doc->save();
-
-            $ref = new CouponRefDocument();
-            $ref->id_coupon_data_main = $id_coupon_data_main;
-            $ref->id_document_data_main = $doc->id;
-            $ref->save();
+            if($ref == null){
+                $doc = New DocumentDataMain();
+                $doc->id_document_const_type = 4;
+                $doc->file_name = $file_name;
+                $doc->save();
+    
+                $ref = new CouponRefDocument();
+                $ref->id_coupon_data_main = $id_coupon_data_main;
+                $ref->id_document_data_main = $doc->id;
+                $ref->save();
+            }
+            
             $filePath = "public/coupons/".$id_coupon_data_main."/".$file_name;
             Storage::disk('local')->put($filePath, file_get_contents($file));
         }
