50f0dc289124f57e957e3de00837d271988dfb4f
krosfiin <mszymaniec@interia.eu>
2020-01-17 19:42:38 +0100

    mm

--- app/Listeners/LogSuccessfullLogin.php
+++ app/Listeners/LogSuccessfullLogin.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\User;
 use Laravel\Passport\Events\AccessTokenCreated;
 use Illuminate\Http\Request;
+use Laravel\Passport\Token;
 
 class LogSuccessfullLogin
 {
@@ -27,6 +28,14 @@ class LogSuccessfullLogin
      */
     public function handle(AccessTokenCreated $event)
     {
+        //
+        Token::where('id', '!=', $event->tokenId)
+        ->where('user_id', $event->userId)
+        ->where('client_id', $event->clientId)
+        // ->where('expires_at', '<', Carbon::now())
+        // ->orWhere('revoked', true)
+        ->delete();
+        
         $user = User::find($event->userId);
         $user->last_login_date = date('Y-m-d H:i:s');
         $user->save();
