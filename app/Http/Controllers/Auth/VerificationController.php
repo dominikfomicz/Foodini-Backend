71b59dabff598058b6b7cb11f1157a81ab7fc64e
Dominik Fomicz <admin@nc-dev.pl>
2020-02-01 13:04:39 +0100

    sad

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class VerificationController extends Controller
 {
