14022b7594d905be278db1ca8363f3da63533b6f
Dominik Fomicz <admin@nc-dev.pl>
2020-02-01 13:11:58 +0100

    qwe

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Verified;
 use App\User;
 
 class VerificationController extends Controller
