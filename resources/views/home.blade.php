b1768635a57f1f2a520f74c6a762175a5deb747c
Dominik Fomicz <admin@nc-dev.pl>
2020-02-11 17:26:15 +0100

    xc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Komunikat</div>
 
-                <div class="card-body">
+                <div class="card-body" style="text-align: center">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
