86c983e7b51b6b146b6dc8654ef49d72d78be6d6
Dominik Fomicz <admin@nc-dev.pl>
2020-02-11 17:12:20 +0100

    dsf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,6 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                         <a href="{{ url('/log-viewer/logs') }}">Logs</a>
-                        {{ Auth::user()->user_type }}
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
