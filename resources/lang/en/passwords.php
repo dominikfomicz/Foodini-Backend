b007ddb3581599d1c790e28bb3b462c28c299aba
Dominik Fomicz <admin@nc-dev.pl>
2020-02-11 17:54:32 +0100

    df

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'reset' => 'Hasło zostało zresetowane.',
+    'sent' => 'Sprawdź skrzynkę i postępuj zgodnie z instrukcjami',
+    'throttled' => 'Za dużo rządań!',
+    'token' => 'Token jest nieprawidłowy',
+    'user' => "Nie mamy w bazie takiego adresu email.",
 
 ];
