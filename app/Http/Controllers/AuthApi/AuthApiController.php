7d47e54ab7e644c668d5cb3213df1f341b24788c
Dominik Fomicz <nyhm801@gmail.com>
2020-03-30 17:38:40 +0200

    dfz

--- app/Http/Controllers/AuthApi/AuthApiController.php
+++ app/Http/Controllers/AuthApi/AuthApiController.php
@@ -10,88 +10,92 @@ use Auth;
 
 class AuthApiController extends Controller
 {
-	public function generateAccessToken($user){
-		$token = $user->createToken($user->email.'-'.now());
+    public function generateAccessToken($user)
+    {
+        $token = $user->createToken($user->email . '-' . now());
 
-		return $token->accessToken;
-	}
+        return $token->accessToken;
+    }
 
 
-	public function register(Request $request){
+    public function register(Request $request)
+    {
         $validate = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required',
 
-		]);
+        ]);
 
-		if($validate->fails()){
-			return -1;
-		}
+        if ($validate->fails()) {
+            return -1;
+        }
 
-		$user = User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
             'user_type' => 1
-		]);
+        ]);
 
         return 0;
-	}
+    }
 
-	public function registerUuid(Request $request){
-		$validate = Validator::make($request->all(), [
-			'uuid' => 'required|email|unique:users'
-		]);
+    public function registerUuid(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'uuid' => 'required|unique:users'
+        ]);
 
-		if($validate->fails()){
-			return -1;
-		}
+        if ($validate->fails()) {
+            return -1;
+        }
 
-		$user = User::create([
+        $user = User::create([
             'name' => $request->uuid,
             'email' => $request->uuid,
             'password' => bcrypt($request->uuid),
             'user_type' => 0
-		]);
+        ]);
 
         return 0;
-	}
+    }
 
-	public function getUserStatus(Request $request){
-		$request->validate([
+    public function getUserStatus(Request $request)
+    {
+        $request->validate([
             'uuid' => 'required'
-		]);
+        ]);
 
 
-		$user= User::where('email', $request->uuid)->first();
+        $user = User::where('email', $request->uuid)->first();
 
-		if($user != null){
-			$user_type = $user->user_type;
-		}else{
-			$user_type = 0;
-		}
+        if ($user != null) {
+            $user_type = $user->user_type;
+        } else {
+            $user_type = 0;
+        }
 
         return $user_type;
-	}
+    }
 
-	public function registerEmail(Request $request){
-		$request->validate([
+    public function registerEmail(Request $request)
+    {
+        $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
-			'password' => 'required'
-		]);
-		//mail like msc@msc is correct
+            'password' => 'required'
+        ]);
+        //mail like msc@msc is correct
 
-		$user = User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
             'user_type' => 1
-		]);
-		$user->sendEmailVerificationNotification();
+        ]);
+        $user->sendEmailVerificationNotification();
 
         return 0;
-	}
-
+    }
 }
